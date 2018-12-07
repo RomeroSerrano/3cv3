3fe650bf928e4885f1098427c3a4fb282c0aa7a2
LarzRS <larz_rs@hotmail.com>
2018-12-07 14:52:38 -0600

    Agregado filtro de Login

---
+++ src/main/java/mx/ipn/escom/wad/tarea6/filter/LoginFilter.java
@@ -0,0 +1,63 @@
+package mx.ipn.escom.wad.tarea6.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import mx.ipn.escom.wad.tarea6.exception.NombreObjetosSession;
+
+/**
+ * Servlet Filter implementation class LoginFilter
+ */
+@WebFilter("/LoginFilter")
+public class LoginFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public LoginFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+
+		HttpSession session = req.getSession(false);
+		boolean loggedin = session != null && session.getAttribute(NombreObjetosSession.USER) != null;
+		// pass the request along the filter chain
+		if(loggedin) {
+			chain.doFilter(request, response);
+		} else {
+			resp.sendRedirect("/3cv3/");			
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
